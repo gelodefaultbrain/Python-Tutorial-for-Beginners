2e42985ad2a4084abd370618e991e6aab94a9a7b
Gels <55988471+gelodefaultbrain@users.noreply.github.com>
2020-08-22 12:20:37 +0800

    Add files via upload

---
+++ Python Lesson 15 print with format specifier.py
@@ -0,0 +1,16 @@
+'''
+%s - string data type
+%i and %d - integer data type
+%f - float data type
+
+'''
+
+
+var_int = 1
+var_float = 1.2
+var_bool = True
+var_string = "Subscribe!"
+
+
+#Python 2
+print("The value is %i %.4f" % (var_int,var_float))
