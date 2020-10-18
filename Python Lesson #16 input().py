64adbc08fff4306677af62c9c49cd8f564425442
Gels <55988471+gelodefaultbrain@users.noreply.github.com>
2020-10-18 14:18:05 +0800

    Add files via upload

---
+++ Python Lesson #16 input().py
@@ -0,0 +1,12 @@
+data = input("What is your data? ")
+
+print("Hello " + data + "\n")
+print("The data type of this is: ",type(data))
+
+
+converted_data = bool(data)
+
+print(converted_data)
+print(type(converted_data))
+
+
