19987ed69758469fcb521ec5b5fd613048e1bacd
Angelo <emberemberspirit@gmail.com>
2020-10-18 14:48:20 +0800

    Python beginner tutorials first commits

---
+++ Python Lesson #13 print() function p1.py
@@ -0,0 +1,13 @@
+var_int = 100
+var_float = 0.0101
+var_bool = True
+var_string = "Gels! Subscribe!"
+
+#end
+#sep
+#print("Hello","World",var_string,end="\n")
+
+#print("Hello\n\n")
+#print("World")
+
+print(print())
