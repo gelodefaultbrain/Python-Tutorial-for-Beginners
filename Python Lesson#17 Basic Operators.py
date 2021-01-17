cf7fcfed15ddf330d41527c899d860f04a99a050
Angelo <emberemberspirit@gmail.com>
2021-01-17 15:43:56 +0800

    Added py lesson 17

---
+++ Python Lesson#17 Basic Operators.py
@@ -0,0 +1,11 @@
+'''
+Basic Operators - + - / *
+Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+'''
+
+#Floor-division -> https://stackoverflow.com/questions/37082597/difference-between-and-in-python-2
+#n+=1 vs n = n + 1 -> https://stackoverflow.com/questions/41446833/what-is-the-difference-between-i-i-1-and-i-1-in-a-for-loop
+
+
+s = "Gels "
+print(s*5)
