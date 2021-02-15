0caa33b4db8026ee031997e66e32f8e65316a771
Angelo <emberemberspirit@gmail.com>
2021-02-15 16:00:05 +0800

    added python lesson 18

---
+++ Python Lesson#18 Comparison Operators.py
@@ -0,0 +1,21 @@
+'''
+== equal or other's say equal equal
+
+!= not equal
+
+> greater than
+
+< less than
+
+>= greater than or equal
+
+<= less than or equal
+
+'''
+
+number = 10
+					  #FALSE           #TRUE
+result = number <= 10 #number < 10 OR number == 10
+
+
+print("Result is: ",result)
