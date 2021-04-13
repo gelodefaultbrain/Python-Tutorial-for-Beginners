43a387ec19302b87d74b630044a5ad0269ea4c16
Gels <emberemberspirit@gmail.com>
2021-04-14 00:46:17 +0800

    py added lesson #19

---
+++ Python Lesson#19 Logical Operators.py
@@ -0,0 +1,20 @@
+
+n1 = 1
+n2 = 2
+
+n3 = 10
+n4 = 20
+
+'''
+Logical
+
+AND - 	Returns True if both statements are true
+OR - 	Returns True if one of the statements is true	
+NOT - 	Reverse the result, returns False if the result is true
+'''
+
+condition = n1 > n2 or n3 < n4
+            #FALSE      #TRUE
+
+print(condition)
+print(not condition)
